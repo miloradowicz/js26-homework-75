eaff4041e13687ecd2c317ad2ab8cda5e76e9608
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-12-05 12:00:22 +0600

    api updated cors allowed

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import encodeRouter from './routers/encode';
 import decodeRouter from './routers/decode';
@@ -6,6 +7,7 @@ import decodeRouter from './routers/decode';
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/encode', encodeRouter);
 app.use('/decode', decodeRouter);
