5377b234332bbe87d93c0b4918b2c113679af26f
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-12-05 12:01:08 +0600

    frontend initialized, packages installed

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,12 @@
+export interface Message {
+  password: string;
+  message: string;
+}
+
+export interface EncodedMessage {
+  encoded: string;
+}
+
+export interface DecodedMessage {
+  decoded: string;
+}
